a0866e999193e2a33c285dbc8a13ec0351863c83
Philippe Roemer <philipperoemer@yahoo.com>
2022-10-26 09:15:05 -0400

    Firestore package added and test dashboard page updated. App is now connected and pulls test data from Firestore collections

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Logo from "../../src/Images/eFormLogo.png";
+import { Link } from "react-router-dom";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function Dashboard() {
-    return <div>Dashboard</div>;
+    const [forms, setForms] = useState([]);
+
+    useEffect(() => {
+        const getForms = async () => {
+            const data = await getDocs(collection(db, "user", "rh87ciX1RjB0V2CYX4vi", "forms"));
+            console.log("Data docs:");
+            console.log(data);
+            console.log(data.docs);
+            const allForms = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            console.log(allForms);
+            setForms(allForms);
+            console.log(allForms[0].questions.question1);
+        };
+        getForms();
+    }, []);
+
+    return (
+        <div>
+            <img src={Logo} alt="" className="logo25" />
+
+            <Link to="/form" className="defaultButton">
+                Create a new form
+            </Link>
+            <p>Recent</p>
+            <p>My forms</p>
+            {forms.map((form) => (
+                <>
+                    <p>{form.form_name}</p>
+                    <p>{form.id}</p>
+                </>
+            ))}
+        </div>
+    );
 }
 
 export default Dashboard;
