4d405b5293faa08b500447c14e74a25dfea431e3
Philippe Roemer <philipperoemer@yahoo.com>
2022-10-27 18:48:41 -0400

    Firestore data is now displayed on generated form pages. Outline added.

--- src/Pages/GeneratedForm.js
+++ src/Pages/GeneratedForm.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Logo from "../../src/Images/eFormLogo.png";
+import { db } from "../firebase-config";
+import { doc, getDoc } from "firebase/firestore";
 
 function GeneratedForm() {
-    let { formID } = useParams();
+    const [formData, setFormData] = useState("");
+    const [formQuestions, setFormQuestions] = useState([]);
+
+    const { formID } = useParams();
+
+    useEffect(() => {
+        const getLinks = async () => {
+            const docRef = doc(db, "user", "rh87ciX1RjB0V2CYX4vi", "forms", formID);
+            const docData = await getDoc(docRef);
+            const data = docData.data();
+
+            setFormData(data);
+            setFormQuestions(data.questions);
+        };
+        getLinks();
+    }, []);
     return (
         <div>
+            <img src={Logo} alt="" />
             <h1>Gemerated Form Page - {formID}</h1>
+            <h1>{formData.form_name}</h1>
+            {formQuestions.map((data) => (
+                <>
+                    <p>{data.question}</p>
+                    <p>{data.type}</p>
+                </>
+            ))}
         </div>
     );
 }
